0ed301e45f2406a41f87a4162c72430aaa1f7ca3
Aldaa11 <167632390+Aldaa11@users.noreply.github.com>
2024-10-22 00:41:05 -0500

    Add files via upload
    Inicializando proyecto.

---
+++ src/main/java/com/edu/cibertec/i20220574/I20220574Application.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.i20220574;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I20220574Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I20220574Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cibertec/i20220574/I20220574ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.i20220574;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I20220574ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
