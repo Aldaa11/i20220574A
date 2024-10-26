f8054b61d315c240aa29837d5a9d895be74c9cc8
Aldaa11 <167632390+Aldaa11@users.noreply.github.com>
2024-10-25 23:28:57 -0500

    Update ClientesController.java

--- src/main/java/com/edu/cibertec/i20220574/controllers/ClientesController.java
+++ src/main/java/com/edu/cibertec/i20220574/controllers/ClientesController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-/*@RestController*/
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
